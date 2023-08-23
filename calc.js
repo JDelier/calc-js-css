e349e378885b59041ca6181bac968d652ff837e3
JDelier <jdelierr@gmail.com>
2023-08-22 22:21:50 -0300

    first_commit 2208

---
+++ calc.js
@@ -0,0 +1,25 @@
+
+export function soma(n1,n2) {
+ 
+    return  n1 + n2
+   
+}
+
+export function subtracao(n1, n2) {
+    const result = n1 - n2
+    return result
+}
+
+export  function multiplicacao(n1, n2) {
+
+    const result = n1 * n2
+    return result
+
+}
+
+export function divisao(n1, n2) {
+
+    const result = n1 / n2
+    return result
+
+}
